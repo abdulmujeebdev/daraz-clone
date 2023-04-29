f6d0ecff3c69e792c494e247030259138a342b6e
cc-abdulmujeeb <abdul.mujeeb@cooperativecomputing.com>
2023-04-29 18:37:13 +0500

    init

---
+++ backoffice/config.php
@@ -0,0 +1,36 @@
+<?php
+// APPLICATION
+define('APPLICATION', 'Admin');
+
+// HTTP
+define('HTTP_SERVER', 'http://opencart.me/backoffice/');
+define('HTTP_CATALOG', 'http://opencart.me/');
+
+// DIR
+define('DIR_OPENCART', '/home/abdul/Sites/opencart/');
+define('DIR_APPLICATION', DIR_OPENCART . 'backoffice/');
+define('DIR_EXTENSION', DIR_OPENCART . 'extension/');
+define('DIR_IMAGE', DIR_OPENCART . 'image/');
+define('DIR_SYSTEM', DIR_OPENCART . 'system/');
+define('DIR_CATALOG', DIR_OPENCART . 'catalog/');
+define('DIR_STORAGE', '/home/abdul/Sites/storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'mujeeb');
+define('DB_PASSWORD', 'Aqua11212211!');
+define('DB_DATABASE', 'eshop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
